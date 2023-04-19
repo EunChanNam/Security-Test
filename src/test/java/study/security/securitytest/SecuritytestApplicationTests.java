a03d6db922f24e153e5068071746445ff733eac9
EunChanNam <namhm23@gmail.com>
2023-04-19 13:23:44 +0900

    initial commit

---
+++ src/test/java/study/security/securitytest/SecuritytestApplicationTests.java
@@ -0,0 +1,13 @@
+package study.security.securitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
